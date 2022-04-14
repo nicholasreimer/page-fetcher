a9350957a1164ce6eb4c0292573c352ca2a514e2
nicholasreimer <nchlsreimer@gmail.com>
2022-04-14 23:03:40 +0000

    new file

---
+++ fetcher.js
@@ -0,0 +1,12 @@
+/* ASSIGNMENT:
+Implement a node app called fetcher.js.
+
+It should take two command line arguments:
+1.) a URL
+2.) a local file path
+
+-It should download the resource at the URL to the local path on your machine. 
+-Upon completion, it should print out a message like:
+ Downloaded and saved 1235 bytes to ./index.html.
+*/
+
